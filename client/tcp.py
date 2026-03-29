d71d1f66beb7c368624b0f104a8097fa2c7b7137
Roee Hashai <roee.hashai@gmail.com>
2026-03-30 00:58:18 +0300

    updated tcp send function

--- client/tcp.py
+++ client/tcp.py
@@ -3,13 +3,7 @@ class Tcp:
     # data comes in bytes just like the sys call send
     @staticmethod
     def send(conn, data):
-        try:
-            conn.sendall(len(data).to_bytes(4, 'big') + data)
-        except (BrokenPipeError, ConnectionResetError) as e:
-            logging.warning(f"Error in connection occurred while sending data: {e}, killing client")
-            self.kill(conn)
-        except Exception as e:
-            logging.warning(f"Error occurred while sending data: {e}")
+        conn.sendall(len(data).to_bytes(4, 'big') + data)
     # data comes in bytes just like the sys call recv
     @staticmethod
     def recive(conn):
